38eef7bef5feac83f75547a33861441349cfc40d
Daniil Panov <from4life@pm.me>
2023-08-01 18:31:42 +0700

    Ship function takes coordinates and determines the length of the ship

--- src/index.js
+++ src/index.js
@@ -1,20 +1,40 @@
 console.log("so it begins...");
 
-const Ship = (length) => {
-  let shipLength = length;
+const Ship = (coordinates) => {
+  let shipLength = coordinates.length;
+  const location = [...coordinates];
+
+  const isSunk = () => console.log("Ship is sunk!");
 
   const wasHit = () => {
+    console.log(shipLength);
     shipLength -= 1;
-  };
-
-  const isSunk = () => {
     if (shipLength === 0) {
-      return console.log("Ship is sunk!");
+      isSunk();
     }
   };
+
+  return { location, wasHit };
 };
 
-const Gameboard = () => {
+const ship1 = Ship([
+  [2, 2],
+  [2, 3],
+]);
+const ship2 = Ship([
+  [2, 5],
+  [2, 6],
+  [2, 7],
+]);
+
+console.log(ship1);
+ship1.wasHit();
+ship1.wasHit();
+
+const Gameboard = (ship) => {
+  const shipsLocations = [];
+  const missedShots = [];
+  const sunkenShips = [];
   // gameboard should have two coordinates X and Y which are represented by numbers
   // from 0 to 9, and look like [0,0], [0,1]... next line [1,0], [1,1]...
   const createBoard = () => {
@@ -28,8 +48,13 @@ const Gameboard = () => {
     }
     return board;
   };
+  const receiveAttack = (coordinates) => {
+    // if coordinates in the shipsLocations - mark as hit
+    // else add it to missedShots
+    // if ship is destroyed add it to sunken ships
+  };
   return { createBoard };
 };
 
-const board = Gameboard();
-console.log(board.createBoard());
+// const board = Gameboard();
+// console.log(board.createBoard());
